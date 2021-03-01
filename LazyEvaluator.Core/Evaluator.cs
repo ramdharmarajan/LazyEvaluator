aa58136de1091f0f5c53eedd5cdf3d1e310b865f
Ram <ramakrishna.d@gmail.com>
2021-03-01 01:54:46 +0000

    First Passing Function Test

---
+++ LazyEvaluator.Core/Evaluator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LazyEvaluator.Core
+{
+    public class Evaluator<T> where T : struct
+    {
+        private readonly List<(Func<T, T[], T>, T[])> _expressions = new();
+        
+        public void Add(Func<T, T[], T> func, params T[] additionalArgs)
+        {
+            _expressions.Add((func, additionalArgs));
+        }
+
+        public T Evaluate(T seed)
+        {
+            _expressions.ForEach(expression =>
+            {
+                var args = expression.Item2;
+
+                seed = expression.Item1.Invoke(seed, args);
+            });
+
+            return seed;
+        }
+    }
+}
---
+++ LazyEvaluator.Tests/EvaluatorShould.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using LazyEvaluator.Core;
+using NUnit.Framework;
+
+namespace LazyEvaluator
+{
+    public class EvaluatorShould
+    {
+        [Test]
+        public void CalculateTheCorrectOutput()
+        {
+            Evaluator<int> evaluator = new Evaluator<int>();
+            
+            evaluator.Add((val, additionalVals) => val / 2);
+            evaluator.Add((val, additionalVals) => val + additionalVals[0], 5);
+            evaluator.Add((val, additionalVals) => val + 1 + additionalVals[0], 20);
+            evaluator.Add((val, additionalVals) => val - additionalVals[0] - additionalVals[1], 5, 3);
+
+            evaluator.Evaluate(8).Should().Be(22);
+        }
+    }
+}
